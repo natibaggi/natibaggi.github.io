024c04e6a6259fe9cf435c8ba334f349c4705862
Natalia Baggi <natibaggi@gmail.com>
2020-09-20 20:36:31 -0300

    exe if, elif, else

---
+++ estudos/python/Aula_11.09/lista-exe4.py
@@ -0,0 +1,12 @@
+num1 = float(input("Digite o 1o número: "));
+num2 = float(input("Digite o 2o número: "));
+num3 = float(input("Digite o 3o número: "));
+
+if num1 > num2 and num1 > num3:
+    print ("Numero 1 é maior")
+elif num2 > num1 and num2 > num3:
+    print ("Numero 2 é maior")
+else:
+    print ("Numero 3 é maior")
+
+
