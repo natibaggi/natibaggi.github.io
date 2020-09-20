b4cba73d430d0871a32482385e184deec1f33494
Natalia Baggi <natibaggi@gmail.com>
2020-09-20 20:34:55 -0300

    exe while contador

---
+++ estudos/python/Aula 18.09/exemplo2_A.py
@@ -0,0 +1,10 @@
+contador = 0;
+
+caracter = input("Digite uma letra: ")
+
+while caracter != "F":
+
+    caracter = input("Digite uma letra: ")
+    contador = contador + 1
+
+print("Quantidade de letra informadas:", contador)
