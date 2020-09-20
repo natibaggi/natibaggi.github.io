6aefb2ef82f1492c6cc238aa3b89eaddbe46c46d
Natalia Baggi <natibaggi@gmail.com>
2020-09-20 20:37:00 -0300

    exe for

--- estudos/python/Lista 2/exemplo.py
+++ estudos/python/Lista 2/exemplo.py
@@ -1,2 +1,18 @@
 print("natalia", "baggi", sep= "/")
 
+total = 3
+
+for contador in range(1,total + 1):
+    print(contador)
+
+print("********")
+
+for contador in range(1,2,3):
+    print(contador)
+
+print("********")
+
+for contador in (1,2,3,4):
+    print(contador)
+
+print("********")
